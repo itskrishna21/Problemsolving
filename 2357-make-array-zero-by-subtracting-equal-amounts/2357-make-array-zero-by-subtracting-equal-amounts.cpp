6379fc3dc7a25dd38ae1cada91a1f8d810d34d77
Krishna <71915757+itskrishna21@users.noreply.github.com>
2022-08-01 00:42:10 +0530

    Time: 0 ms (100.00%), Space: 8.6 MB (100.00%) - LeetHub

--- 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.cpp
+++ 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.cpp
@@ -2,8 +2,50 @@ class Solution {
 public:
     int minimumOperations(vector<int>& nums) {
         
-        int ans=0;
+//         int ans=0;
         
+//         int count_zero=0;
+        
+//         for(auto i:nums){
+//             if(i==0){
+//                 count_zero++;
+//             }
+//         }
+        
+//         int size=nums.size();
+        
+//         if(count_zero==size){
+//             return 0;
+//         }
+        
+//         size-=count_zero;
+        
+//         while(count_zero!=size){
+//             ans++;
+//             int min_element=INT_MAX;
+//             for(auto i:nums){
+//                 if(i>0 && i<min_element){
+//                     min_element=i;
+                    
+//                 }
+//             }
+//             int zeroes=0;
+//             for(int i=0;i<nums.size();i++){
+//                 if(nums[i]>0)
+//                 {
+//                     nums[i]=nums[i]-min_element;
+//                 }
+//                 if(nums[i]==0){
+//                     zeroes++;
+//                 }
+//             }
+//             if(zeroes==nums.size()){
+//                 count_zero=size;
+//             }
+//         }
+//         return ans;
+        
+        // approach 2
         int count_zero=0;
         
         for(auto i:nums){
@@ -12,38 +54,7 @@ public:
             }
         }
         
-        int size=nums.size();
-        
-        if(count_zero==size){
-            return 0;
-        }
-        
-        size-=count_zero;
-        
-        while(count_zero!=size){
-            ans++;
-            int min_element=INT_MAX;
-            for(auto i:nums){
-                if(i>0 && i<min_element){
-                    min_element=i;
-                    
-                }
-            }
-            cout<<min_element<<" ";
-            int zeroes=0;
-            for(int i=0;i<nums.size();i++){
-                if(nums[i]>0)
-                {
-                    nums[i]=nums[i]-min_element;
-                }
-                if(nums[i]==0){
-                    zeroes++;
-                }
-            }
-            if(zeroes==nums.size()){
-                count_zero=size;
-            }
-        }
-        return ans;
+        unordered_set<int>s(nums.begin(),nums.end());
+        return s.size()-s.count(0);
     }
 };
