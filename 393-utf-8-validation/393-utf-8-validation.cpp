e51c4bb705237a63abfe30eedde0ce9a98b418b2
Krishna <71915757+itskrishna21@users.noreply.github.com>
2022-09-14 04:14:52 +0530

    Time: 31 ms (20.45%), Space: 14.1 MB (38.90%) - LeetHub

---
+++ 393-utf-8-validation/393-utf-8-validation.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool validUtf8(vector<int>& data) {
+        int count = 0;
+        
+        for(auto x:data){
+            if(count==0){
+                if(x>>5==0b110){
+                    count=1;
+                }
+                else if(x>>4==0b1110){
+                    count=2;
+                }
+                else if(x>>3==0b11110){
+                    count=3;
+                }
+                else if(x>>7!=0b0){
+                    return false;
+                }
+            }
+            else{
+                if(x>>6!=0b10){
+                    return false;
+                }
+                count--;
+                
+            }
+        }
+        if(count==0){
+            return true;
+        }
+        return false;
+    }
+};
