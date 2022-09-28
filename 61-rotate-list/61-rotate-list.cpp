11fe75c281f36cd43658961f3cd1e7d72bd83087
Krishna <71915757+itskrishna21@users.noreply.github.com>
2022-09-28 11:48:30 +0530

    Time: 21 ms (12.38%), Space: 11.8 MB (6.58%) - LeetHub

---
+++ 61-rotate-list/61-rotate-list.cpp
@@ -0,0 +1,71 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* rotateRight(ListNode* head, int k) {
+    
+        int size = 0;
+        
+        ListNode* t = head;
+        
+        while(t!=NULL)
+        {
+            size++;
+            t = t->next;
+        }
+        
+        if(size==0)
+        {
+            return head;
+        }
+        
+        k = k%size;
+        
+        cout<<k;
+        
+        if(k==0)
+        {
+            return head;
+        }
+        
+        ListNode* fast = head;
+        ListNode* slow = head;
+        
+        
+        while(k!=0)
+        {
+            fast = fast->next;
+            k--;
+        }
+        
+        while(fast->next!=NULL)
+        {
+            slow = slow->next;
+            fast =fast->next;
+        }
+        
+        ListNode* store = slow->next;
+        slow->next = NULL;
+        ListNode* temp = store;
+        
+        while(temp!=NULL && temp->next!=NULL)
+        {
+            temp = temp->next;
+        }
+        
+        if(temp!=NULL)
+        {
+            temp->next = head;
+        }
+        
+        return store;
+    }
+};
